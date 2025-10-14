bea3d2449f970290d9529045c353ea5d3f6a8297
robshow <robsonaraujocarmo@hotmail.com>
2025-10-14 16:46:18 -0300

    feat: generate Pure css

--- src/features/home/components/FinalCTA.tsx
+++ src/features/home/components/FinalCTA.tsx
@@ -22,7 +22,7 @@ const FinalCTA = () => {
           <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
             <div className="rounded-md shadow">
               <a
-                href="/contact"
+                href="/contact-us"
                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-brand-800 hover:bg-brand-700 md:py-4 md:text-lg md:px-10"
               >
                 Free Consultation
